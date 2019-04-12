f6ff520862f0c6a7fca186d70d2afcb4341072d3
EDUARDOGONCALVE\EduardoGoncalves <e.goncalves@elojr.com.br>
2019-04-12 08:18:25 -0300

    Adicao da versao inicial do teclado

--- app/controllers/condicao1.js
+++ app/controllers/condicao1.js
@@ -1,3 +1,6 @@
 module.exports.condicao1 = function(app,req,res){       
          res.render('condicao1',{ validacao: [], err: ''});
 }
+module.exports.experimento1 = function(app,req,res){       
+    res.render('experimento1',{ validacao: [], err: ''});
+}
--- app/routes/condicao1.js
+++ app/routes/condicao1.js
@@ -1,13 +1,14 @@
 module.exports = function(app){
 
-	app.get('/redirect',function(req, res){
-		app.app.controllers.condicao1.condicao1(app,req,res);
+	app.get('/experimento1',function(req, res){
+		app.app.controllers.condicao1.experimento1(app,req,res);
+		
 	});
+
 
 	app.get('/condicao1',function(req, res){
 		app.app.controllers.condicao1.condicao1(app,req,res);
 		
 	});
-
 
 }
